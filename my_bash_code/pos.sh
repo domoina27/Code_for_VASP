75933d60e32a5f425f1c3be7d6c1527f0b801920
Psalm116 <58156737+domoina27@users.noreply.github.com>
2020-07-30 12:32:35 -0500

    Update pos.sh

--- my_bash_code/pos.sh
+++ my_bash_code/pos.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 
 
 sed -i 's/Direct/S\nDirect/g' POSCAR   # replace Direct with S\nDirect
@@ -7,19 +7,18 @@ line_t=$(wc -l < POSCAR)
 sed -n -e 11,$(expr $line_t)p POSCAR | awk '{print $3}' # read column 3 from line 11 to 116
 
 
-filename='table'
-limit=`echo 0.1 + 0.001 |bc`
+file='table'
+lim=`echo 0.1 |bc`
 n=1
-while read Line; do
-z=$(awk '{print $3}' table |sed -n $(expr $n)p )
-Z=`echo $z + 0.000001|bc`
-if [ $Z>$limit ] 
-        then
-                echo "Line $n is larger than $limit"
-                echo "$Z"
-        else
-                echo "Line $n is smaller than $limit"
-                echo $Z
-        fi
-n=$((n+1))
-done < $filename
+while ISF= read -r line
+do
+   z=$(awk '{print $3}' table |sed -n $(expr $n)p)
+   if [ $(echo "$z>$lim" |bc) -gt 0 ]
+       then
+            echo $z is greater than 0.1
+       else
+            echo "$z is smaller than 0.1"
+   fi
+   n=$((n+1))
+done<$file
+
